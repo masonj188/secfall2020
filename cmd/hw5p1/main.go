d7ce6d0bb87c5ebd6e865ddd3382421bfb9405f5
Mason Johnson <mason@invariant.dev>
2020-09-28 14:45:26 -0600

    Added frequency analysis

--- cmd/hw5p1/main.go
+++ cmd/hw5p1/main.go
@@ -51,3 +51,18 @@ func main() {
 	}
 
 }
+
+/*
+Complexity Analysis
+
+"N" is the number of characters in the input.
+
+Reading from stdin takes "N" time.
+Converting input to lowercase takes "N" time.
+Writing lowercase ascii characters to new string takes "N" time (and <="N" space).
+Filling initial frequency map takes constant (26 operations) time.
+Filling frequency map with total occurrences of each character of input takes "N" time.
+Outputting the difference in frequencies of each character takes constant time (another 26 operations).
+
+Total time = O(4N) + O(1) = O(n)
+*/
